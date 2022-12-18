8c44650936d2b3869543f13f138779a260ce6ab6
nekoo <nereapena42gmail.com>
2022-12-18 22:49:00 +0100

    empezando con ejemplos de diccionarios

---
+++ 08_dict_and_json.py
@@ -0,0 +1,11 @@
+mi_lista = ["hola", "como", "estas"]  # corchetes
+print(mi_lista[2])
+
+persona = {  # llaves / clave -> valor
+    "nombre": "luis",
+    "edad": 25,
+    "altura": 1.75
+}
+print(persona['nombre'])
+print(persona['edad'])
+print(persona['altura'])
